b81cb05606d8a2129922193c54daeabffba52e0a
Alberto Giardina <giardinaalberto@gmail.com>
2023-12-16 03:32:37 +0100

    Inizializzazione Progetto

---
+++ CatalogoOnline/src/views/About.vue
@@ -0,0 +1,13 @@
+<!-- src/views/About.vue -->
+<template>
+  <div>
+    <h1>About</h1>
+    <router-link to="/">Go to Home</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'About',
+};
+</script>
